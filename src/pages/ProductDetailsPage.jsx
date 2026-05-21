6d8cf05b526b3512851e524bc30cc45f08e29a71
ecommfox <medellint@gmail.com>
2026-05-21 19:57:00 +0100

    First commit

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 
 function ProductDetailsPage() {
@@ -9,15 +11,36 @@ function ProductDetailsPage() {
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
-
+ let {productId} = useParams()
+ console.log(productId)
+ 
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+ useEffect(()=>{
+  axios.get(`https://fakestoreapi.com/products/${productId}`).then((response)=>{
+    const product = response.data
+setProduct(product)
+console.log(response)
 
+  }).catch((error)=>{
+
+   console.warn(error) 
+  })
+
+ },[productId])
 
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    <div className="card">
+      <img src={product.image}/>
+      <h1>{product.title}</h1>
+      <div>
+        <p>{product.description}</p>
+        <div>{product.price}
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
@@ -7,11 +10,35 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(()=>{
+
+    axios.get("https://fakestoreapi.com/products").then((response)=>{
+     const products = response.data     
+     setProducts(products)    
+
+      console.log(response);
+    }).catch((error)=>{
+      console.warn(error)
+    })
+
+  },[]);
 
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      {products.map((item)=>{
+       return (
+      <Link key={item.id} to={`/product/details/${item.id}`}>
+       <div className="card">
+       <img src={item.image} alt={item.title}/>
+       <h2>{item.title}</h2>
+       <div>{item.category}</div>
+       <div>{item.price}$</div>   
+       <div>{item.description}</div>
+       </div>
+       </Link>
+       )
+      } )}
     </div>
   );
 }
